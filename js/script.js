6fd03ef222cd5df6528659848c969e602c80d52c
Adrian Wilk <wilku5991@wp.pl>
2021-05-01 15:13:34 +0200

    created function on script.js

--- js/script.js
+++ js/script.js
@@ -1,28 +1,41 @@
-console.log("Witaj");
+{
+    const welcome = () => {
+        console.log("Hey, Hi, Hello! :)");
+    };
 
-let changeBodyColorButton = document.querySelector(".js-buttonChangeBodyColor");
-let body = document.querySelector("body")
-let themeName = document.querySelector(".js-themeName")
+    const toggleBackground = () => {
+        const body = document.querySelector("body");
+        const themeName = document.querySelector(".js-themeName");
 
-changeBodyColorButton.addEventListener("click", () => {
-    body.classList.toggle("body--special");
-    themeName.innerText = body.classList.contains("body--special") ? "jasne" : "ciemne";
-})
+        body.classList.toggle("body--special");
+        themeName.innerText = body.classList.contains("body--special") ? "jasne" : "ciemne";
+    };
 
-let hideMottoButton = document.querySelector(".js-hideMottoButton");
-let changeMottoTextButton = document.querySelector(".js-mottoTextButton")
-let motto = document.querySelector(".js-motto")
+    const toggleMotto = () => {
+        const changeMottoTextButton = document.querySelector(".js-mottoTextButton");
+        const motto = document.querySelector(".js-motto");
 
-hideMottoButton.addEventListener("click", () => {
-    motto.classList.toggle("hide");
-    changeMottoTextButton.innerText = motto.classList.contains("hide") ? "Pokaż" : "Ukryj";
-})
+        motto.classList.toggle("hide");
+        changeMottoTextButton.innerText = motto.classList.contains("hide") ? "Pokaż" : "Ukryj";
+    };
 
-let hidePhotoButton = document.querySelector(".js-hidePhotoButton");
-let changeImageTextButton = document.querySelector(".js-textImageButton")
-let photo = document.querySelector(".js-image")
+    const togglePhoto = () => {
+        const changeImageTextButton = document.querySelector(".js-textImageButton");
+        const photo = document.querySelector(".js-image");
 
-hidePhotoButton.addEventListener("click", () => {
-    photo.classList.toggle("hide");
-    changeImageTextButton.innerText = photo.classList.contains("hide") ? "Pokaż" : "Ukryj";
-})
+        photo.classList.toggle("hide");
+        changeImageTextButton.innerText = photo.classList.contains("hide") ? "Pokaż" : "Ukryj";
+    };
+
+    const init = () => {
+        const changeBodyColorButton = document.querySelector(".js-buttonChangeBodyColor");
+        const hideMottoButton = document.querySelector(".js-hideMottoButton");
+        const hidePhotoButton = document.querySelector(".js-hidePhotoButton");
+
+        changeBodyColorButton.addEventListener("click", toggleBackground);
+        hideMottoButton.addEventListener("click", toggleMotto);
+        hidePhotoButton.addEventListener("click", togglePhoto);
+        welcome();
+    };
+    init();
+}
